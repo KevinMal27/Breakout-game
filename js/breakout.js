c337f58295e0f547d26678d37a7e0d311889d0f4
KevinMal27 <36044189+KevinMal27@users.noreply.github.com>
2018-03-08 16:40:40 +0000

    Breakout Moving the ball off the wall

---
+++ js/breakout.js
@@ -0,0 +1,43 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10; 
+
+function drawBall() {
+	ctx.beginPath();
+    ctx.arc(x,y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill(); 
+    ctx.closePath();
+}
+
+function draw() {
+	ctx.clearRect(0,0, canvas.width, canvas.height);
+    drawBall();
+	
+	
+    x += dx;
+	y += dy;
+	
+	if(y + dy<0) {
+		dy = -dy;
+	
+	}
+	
+	if(y + dy > canvas.height-ballRadius || y + dy <ballRadius) {
+		dy = -dy;
+	}
+	
+	if(x + dx > canvas.width-ballRadius || x + dx <ballRadius) {
+		dx = -dx;
+	} 
+	
+} 
+
+	
+	setInterval(draw, 10);
