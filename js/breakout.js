a6c90d0bc6bdd02061c1017828b56a6af057b57d
KevinMal27 <36044189+KevinMal27@users.noreply.github.com>
2018-03-14 13:35:35 +0000

    Add files via upload

--- js/breakout.js
+++ js/breakout.js
@@ -2,11 +2,21 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 
+
+// Ball Variables
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10; 
+
+// Paddle Variables
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2; 
+var rightPressed = false;
+var leftPressed = false; 
+
 
 function drawBall() {
 	ctx.beginPath();
@@ -15,11 +25,20 @@ function drawBall() {
     ctx.fill(); 
     ctx.closePath();
 }
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "#0095DD"
+	ctx.fill();
+	ctx.closePath();
+}
+
+
 
 function draw() {
 	ctx.clearRect(0,0, canvas.width, canvas.height);
     drawBall();
-	
+	drawPaddle();
 	
     x += dx;
 	y += dy;
@@ -28,16 +47,49 @@ function draw() {
 		dy = -dy;
 	
 	}
+	//Bounce the ball off three walls - if it drops off the bottom - Game Over!
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+	dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+	   dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+	   alert("GAME OVER");
+	   document.location.reload();
+    } 
+    
+	if(rightPressed && paddleX < canvas.width-paddleWidth) {
+		paddleX += 7;
+	}
+	else if(leftPressed && paddleX > 0) {
+		paddleX -= 7;
+	}
+
+    } 
+
+	document.addEventListener("keydown", keyDownHandler, false);
+	document.addEventListener("keyup", keyUpHandler, false);
 	
-	if(y + dy > canvas.height-ballRadius || y + dy <ballRadius) {
-		dy = -dy;
+	function keyDownHandler(e) {
+		if(e.keyCode == 39) {
+			rightPressed = true;
+		}
+		else if(e.keyCode == 37) {
+			leftPressed = true;
+		}
 	}
 	
-	if(x + dx > canvas.width-ballRadius || x + dx <ballRadius) {
-		dx = -dx;
-	} 
 	
-} 
-
+	function keyUpHandler(e) {
+		if(e.keyCode == 39) {
+			rightPressed = false;
+		}
+		else if(e.keyCode == 37) {
+			leftPressed = false;
+		}
+	}
+	
+	
+	
 	
 	setInterval(draw, 10);
